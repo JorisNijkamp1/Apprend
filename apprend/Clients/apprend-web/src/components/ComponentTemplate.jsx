cb0c759b04eb27f632193593fe45a5ae1e04642b
Gino Verwijs <email2verwijs@gmail.com>
2019-11-20 12:23:50 +0100

    added component template

---
+++ apprend/Clients/apprend-web/src/components/ComponentTemplate.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TemplateComponent = (props) => {
+
+    return (
+
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export const Template = connect(mapStateToProps, mapDispatchToProps)(TemplateComponent);
