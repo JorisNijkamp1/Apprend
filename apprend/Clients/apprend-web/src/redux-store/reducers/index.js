0607bf46f137e4203685037eef450f557efa772d
Gino Verwijs <email2verwijs@gmail.com>
2019-11-20 15:44:11 +0100

    added reducer in index

--- apprend/Clients/apprend-web/src/redux-store/reducers/index.js
+++ apprend/Clients/apprend-web/src/redux-store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import clientReducer from './client-reducer'
+import createDeck from './create-deck'
 
 export const allReducers = combineReducers({
     client: clientReducer,
+    createDeck: createDeck,
 })
