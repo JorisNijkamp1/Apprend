4521ab261eeb6dc77266b06e60783e0f6a8170ec
Gino Verwijs <email2verwijs@gmail.com>
2019-11-20 12:52:10 +0100

    made create deck branch

---
+++ apprend/Clients/apprend-web/src/components/CreateDeckForm/CreateDeckForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const CreateDeckFormComponent = (props) => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export const CreateDeckForm = connect(mapStateToProps, mapDispatchToProps)(CreateDeckFormComponent);
