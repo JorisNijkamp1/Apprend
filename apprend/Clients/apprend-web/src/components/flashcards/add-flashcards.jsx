b6a3edd2996680b9ac17c4ce23a4cdf4d777e477
Aaron van den Berg <a.vdberg98@gmail.com>
2019-11-20 15:45:41 +0100

    Added multiple flashcards

--- apprend/Clients/apprend-web/src/components/flashcards/add-flashcards.jsx
+++ apprend/Clients/apprend-web/src/components/flashcards/add-flashcards.jsx
@@ -3,15 +3,28 @@ import Card from "react-bootstrap/Card";
 import {Container} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import {EditableFlashcard} from "./sub-components/editable-flashcard";
+import {AddFlashcardIcon} from "./sub-components/add-flashcard-icon";
 
-export const Flashcards = () => (
-    <Container>
-        <Card>
-            <Card.Body>
-                <Row>
-                    <EditableFlashcard/>
-                </Row>
-            </Card.Body>
-        </Card>
-    </Container>
-);
+const allFlashcards = () => {
+    let rows = [];
+    for (let i = 0; i < 7; i++) {
+        rows.push(<EditableFlashcard key={i} />);
+    }
+    return rows
+};
+
+export const Flashcards = () => {
+    return (
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Row>
+                        {/*<EditableFlashcard/>*/}
+                        {allFlashcards()}
+                        <AddFlashcardIcon/>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+};
--- apprend/Clients/apprend-web/src/components/flashcards/sub-components/editable-flashcard.jsx
+++ apprend/Clients/apprend-web/src/components/flashcards/sub-components/editable-flashcard.jsx
@@ -1,31 +1,49 @@
 import Col from "react-bootstrap/Col";
 import {Form} from "react-bootstrap";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Card from "react-bootstrap/Card";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+
 
 export const EditableFlashcard = () => {
-    const [flashcardTerm, setFlashcardTerm] = useState('Nieuwe flashcard');
+    const [flashcardTerm, setFlashcardTerm] = useState('New Flashcard');
+    const [flashcardDefinition, setFlashcardDefinition] = useState(null);
+
+    let flashcardData = {
+        term: flashcardTerm,
+        definition: flashcardDefinition,
+    };
 
     return (
-        <Col>
-            <Card border="primary">
-                <Card.Header className={"text-center"}>{flashcardTerm}</Card.Header>
-                <Card.Body>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Term</Form.Label>
-                        <Form.Control type="text" placeholder="Huisdieren" onChange={(e) => setFlashcardTerm(e.target.value)}/>
-                        <Form.Text className="text-muted">
-                            Vul hier de term in waarvan je de definitie wilt leren.
-                        </Form.Text>
-                        <hr/>
-                        <Form.Label>Definitie</Form.Label>
-                        <Form.Control type="text" placeholder="Pets"/>
-                        <Form.Text className="text-muted">
-                            Vul hier de definitie van de hierboven ingevulde term in.
-                        </Form.Text>
-                    </Form.Group>
-                </Card.Body>
-            </Card>
-        </Col>
+        <>
+            <Col xs={12} md={6} lg={4}>
+                <Card border="primary" className={'mb-4 hover-shadow-editable-flashcard'}>
+                    <Card.Header className={"text-center"}>
+                        {flashcardTerm}
+                        <span className={"float-right"}>
+                        <FontAwesomeIcon icon={faTrash} className={'trash-icon'}/>
+                    </span>
+                    </Card.Header>
+                    <Card.Body>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Term</Form.Label>
+                            <Form.Control type="text" placeholder="Huisdieren"
+                                          onChange={(e) => setFlashcardTerm(e.target.value)}/>
+                            <Form.Text className="text-muted">
+                                Vul hier de term in waarvan je de definitie wilt leren.
+                            </Form.Text>
+                            <hr/>
+                            <Form.Label>Definition</Form.Label>
+                            <Form.Control type="text" placeholder="Pets"
+                                          onChange={(e) => setFlashcardDefinition(e.target.value)}/>
+                            <Form.Text className="text-muted">
+                                Enter the definition of the term entered above.
+                            </Form.Text>
+                        </Form.Group>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </>
     )
 };
