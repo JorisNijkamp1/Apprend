eb3f5c78f590c705448107c33a8c7fa146861f42
Jesse Koerhuis <JJA.Koerhuis@student.han.nl>
2019-11-20 15:37:35 +0100

    Moved all tests to __tests__ file

--- apprend/Clients/apprend-web/src/__tests__/RegisterPage.test.js
+++ apprend/Clients/apprend-web/src/__tests__/RegisterPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {RegisterPageComponent} from './RegisterPage';
+import {RegisterPageComponent} from '../components/RegisterPage';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
