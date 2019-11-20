84cff0844f342d9131d8a650a6c15921e77d2905
Joris <jorisnijkamp@gmail.com>
2019-11-20 14:42:00 +0100

    Homepage component/ navigatie component/ footer componenet

---
+++ apprend/Clients/apprend-web/src/components/shared/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as ReactRedux from "react-redux"
+
+const FooterUI = (props) => {
+    return (
+        <>
+            <footer className='footer mt-auto py-4 mt-3 bg-gray text-white'>
+                <div className='container text-center display-3 '>Apprend</div>
+            </footer>
+        </>
+
+    )
+}
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+
+export const Footer = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(FooterUI);
--- apprend/Clients/apprend-web/src/components/shared/navbar/NavigatieBar.jsx
+++ apprend/Clients/apprend-web/src/components/shared/navbar/NavigatieBar.jsx
@@ -2,26 +2,20 @@ import React from "react";
 import * as ReactRedux from "react-redux"
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav"
-import Form from "react-bootstrap/Form"
-import FormControl from "react-bootstrap/FormControl"
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-function NavbarUI(props) {
+import Container from "react-bootstrap/esm/Container";
+
+const NavbarUI = (props) => {
     return (
-        <Row>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Navbar expand="lg" className={"bg-green"}>
+            <Container>
+                <Navbar.Brand className={"text-white"} href="#"><h1>Apprend</h1></Navbar.Brand>
+                <Nav>
+                    <Nav.Link className="text-white pl-30" href="#">Create Deck</Nav.Link>
+                    <Nav.Link className="text-white pl-30" href="#">Login</Nav.Link>
+                    <Nav.Link className="text-white pl-30" href="#">Register</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                    <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar>
-        </Row>
+            </Container>
+        </Navbar>
     )
 }
 
