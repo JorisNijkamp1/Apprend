da2f160c157589e659bc869a2402790caa8ca49f
Jesse Koerhuis <JJA.Koerhuis@student.han.nl>
2019-11-21 16:09:10 +0100

    Set up Redux structure

---
+++ apprend/Clients/apprend-web/src/redux-store/actions/action-types.js
@@ -0,0 +1,1 @@
+export const REGISTER_NEW_USER = 'REGISTER_NEW_USER';
--- apprend/Clients/apprend-web/src/redux-store/reducers/index.js
+++ apprend/Clients/apprend-web/src/redux-store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import clientReducer from './client-reducer'
+import registerReducer from './register-reducer';
 
 export const allReducers = combineReducers({
     client: clientReducer,
+    register: registerReducer
 })
