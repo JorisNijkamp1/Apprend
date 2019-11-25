f0f95fd92297e2cdcf4c8fbee41076452b217633
Jesse Koerhuis <JJA.Koerhuis@student.han.nl>
2019-11-25 10:28:35 +0100

    Removed duplicate endpoint

--- apprend/Server/routes/api/v1/users/users.js
+++ apprend/Server/routes/api/v1/users/users.js
@@ -68,26 +68,4 @@ users.get('/:id/_id', async (req, res) => {
     }
 });
 
-/*====================================
-| GET ALL DECKS FROM A USER
-*/
-decks.get('/:username/decks', async (req, res) => {
-    await Users.findOne({ _id: req.params.username }, function (err, user) {
-        if (user) {
-            return res.json({
-                success: true,
-                decks: {
-                    user: !(user.email && user.password) ? 'anonymous user' : user._id,
-                    decks: user.decks
-                }
-            })
-        }else {
-            return res.json({
-                success: false,
-                error: "User doesn't exist"
-            })
-        }
-    });
-});
-
 module.exports = users;
