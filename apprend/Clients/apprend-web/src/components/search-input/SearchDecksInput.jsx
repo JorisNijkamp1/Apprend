3e3c41ab64d6afaaffc05dc1c65c6656b25fdbb4
Aaron van den Berg <a.vdberg98@gmail.com>
2019-12-05 10:50:11 +0100

    Auto suggest search form

--- apprend/Clients/apprend-web/src/components/search-input/SearchDecksInput.jsx
+++ apprend/Clients/apprend-web/src/components/search-input/SearchDecksInput.jsx
@@ -2,151 +2,124 @@ import React, {useState} from "react";
 import Autosuggest from 'react-autosuggest';
 import {API_URL} from "../../redux-store/urls";
 
-const exampleDecks = ([
-    {
-        name: 'C',
-        flashcards: 3
-    },
-    {
-        name: 'C#',
-        flashcards: 12
-    },
-    {
-        name: 'C++',
-        flashcards: 33
-    },
-    {
-        name: 'Clojure',
-        flashcards: 54
-    },
-    {
-        name: 'Elm',
-        flashcards: 14
-    },
-    {
-        name: 'Go',
-        flashcards: 21
-    },
-    {
-        name: 'Haskell',
-        flashcards: 28
-    },
-    {
-        name: 'Java',
-        flashcards: 39
-    },
-    {
-        name: 'JavaScript',
-        flashcards: 22
-    },
-    {
-        name: 'Perl',
-        flashcards: 11
-    },
-    {
-        name: 'PHP',
-        flashcards: 74
-    },
-    {
-        name: 'Python',
-        flashcards: 3
-    },
-    {
-        name: 'Ruby',
-        flashcards: 23
-    },
-    {
-        name: 'Scala',
-        flashcards: 76
+const getMatchingLanguages = (value, decks) => {
+    const escapedValue = escapeRegexCharacters(value.trim());
+
+    if (escapedValue === '') {
+        return [];
     }
-]);
 
-// Imagine you have a list of decks that you'd like to autosuggest.
-const decks = async () => {
-    const url = `${API_URL}/decks`;
+    const regex = new RegExp('^' + escapedValue, 'i');
 
-    const response = await fetch(url, {
-        credentials: 'include',
-        method: 'GET',
-        // body: JSON.stringify(givenDeck),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-
-    if (response.status === 200) {
-        const data = await response.json();
-        return data.decks
-    } else {
-        console.log('Error: ' + response);
-        return []
-    }
+    return decks.filter(deck => regex.test(deck.name));
 };
 
-// Teach Autosuggest how to calculate suggestions for any given input value.
-const getSuggestions = value => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
+/* ----------- */
+/*    Utils    */
+/* ----------- */
 
-    return inputLength === 0 ? [] : exampleDecks.filter(deck =>
-        deck.name.toLowerCase().slice(0, inputLength) === inputValue
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+/* --------------- */
+/*    Component    */
+/* --------------- */
+
+function getSuggestionValue(suggestion) {
+    return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+    console.log(suggestion)
+    return (
+        <>
+            <span style={{fontWeight: 600}}>{suggestion.name}</span>
+            <br/>
+            <i>{suggestion.totalFlashcards} flashcards</i>
+        </>
     );
-};
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.name;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-    <div>
-        <span style={{fontWeight: 600}}>{suggestion.name}</span>
-        <br/>
-        <i>{suggestion.flashcards} flashcards</i>
-    </div>
-);
+}
 
 export const SearchDecksInput = () => {
     const [value, setValue] = useState('');
     const [suggestions, setSuggestions] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    let lastRequestId = null;
+
+    const loadSuggestions = (value) => {
+        // Cancel the previous request
+        if (lastRequestId !== null) {
+            clearTimeout(lastRequestId);
+        }
+
+        setIsLoading(true);
+
+        // Request
+        lastRequestId = setTimeout(async () => {
+
+            const url = `${API_URL}/decks`;
+            let decks;
+
+            const response = await fetch(url, {
+                credentials: 'include',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (response.status === 200) {
+                const data = await response.json();
+                decks = data.decks
+            } else {
+                console.log('Error: ' + response);
+                decks = []
+            }
+
+            setIsLoading(false);
+            setSuggestions(getMatchingLanguages(value, decks));
+        });
+    };
 
     const onChange = (event, {newValue}) => {
-        return setValue(newValue);
+        setValue(newValue);
     };
 
-    // Autosuggest will call this function every time you need to update suggestions.
-    // You already implemented this logic above, so just use it.
-    const onSuggestionsFetchRequested = async ({value}) => {
-        console.log('onSuggestionsFetchRequested');
-
-        return setSuggestions(getSuggestions(value));
+    const onSuggestionsFetchRequested = ({value}) => {
+        loadSuggestions(value);
     };
 
-    // Autosuggest will call this function every time you need to clear suggestions.
     const onSuggestionsClearRequested = () => {
-        console.log('onSuggestionsClearRequested');
-        return setSuggestions([]);
+        setSuggestions([]);
     };
 
-    // Autosuggest will pass through all these props to the input.
+    const status = (isLoading ? 'Loading...' : 'Type to load suggestions');
+
     const inputProps = {
-        placeholder: 'Search for a Deck',
+        placeholder: "Search a deck",
         value,
         onChange: onChange,
         className: 'form-control',
         style: {width: '100%'}
     };
 
-    // Finally, render it!
     return (
-        <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={onSuggestionsClearRequested}
-            getSuggestionValue={getSuggestionValue}
-            renderSuggestion={renderSuggestion}
-            inputProps={inputProps}
-        />
+        <>
+            {/*<div className="status">*/}
+            {/*    <strong>Status:</strong> {status}*/}
+            {/*</div>*/}
+            <Autosuggest
+                suggestions={suggestions}
+                onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={onSuggestionsClearRequested}
+                getSuggestionValue={getSuggestionValue}
+                renderSuggestion={renderSuggestion}
+                inputProps={inputProps}
+            />
+        </>
     );
-};
+}
+
