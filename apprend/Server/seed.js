421cec930a896f0f793994bbcbb4209c08af1707
Aaron van den Berg <a.vdberg98@gmail.com>
2019-12-05 10:49:23 +0100

    Seed file updated

--- apprend/Server/seed.js
+++ apprend/Server/seed.js
@@ -27,43 +27,81 @@ async function seedUsers() {
             "_id": "Joris",
             "email": "jorisnijkamp@gmail.com",
             "password": "$2a$10$jAeV7PFZ4REdverFjPiDx.3ZNd7tdqdx1VRQ/2ZqQQxL1y.TRE5Gq",
-            "decks": [{
-                "_id": "5ddfadab612b09570c6f3a33",
-                "name": "Frans woordjes",
-                "description": "Mooie lijst met 50 woordjes frans erg gaaf!",
-                "creatorId": "Joris",
-                // "creatorId": ,
-                // "lastPlayedDate": ,
-                "status": "isEdited",
-                "flashcards": []
-            }]
+            "decks": [
+                {
+                    "_id": "5ddfadab612b09570c6f3a33",
+                    "name": "French to Dutch words",
+                    "description": "Prepositions in French",
+                    "creatorId": "Joris",
+                    // "creatorId": ,
+                    // "lastPlayedDate": ,
+                    "status": "isEdited",
+                    "flashcards": []
+                },
+                {
+                    "_id": "5ddfadab612b09570c6f3a33",
+                    "name": "Filipino to Dutch words",
+                    "description": "Learn Filipino words",
+                    "creatorId": "Joris",
+                    // "creatorId": ,
+                    // "lastPlayedDate": ,
+                    "status": "isEdited",
+                    "flashcards": []
+                },
+                {
+                    "_id": "5ddfadab612b09570c6f3a33",
+                    "name": "Finnish to Dutch prefixes",
+                    "description": "Learning prefixes ",
+                    "creatorId": "Joris",
+                    // "creatorId": ,
+                    // "lastPlayedDate": ,
+                    "status": "isEdited",
+                    "flashcards": [
+                        {
+                            "_id": "Apprende0",
+                            "type": "Text only",
+                            "question": "Kissa",
+                            "answer": "Kat"
+                        },
+                        {
+                            "_id": "Apprende1",
+                            "type": "Text only",
+                            "question": "Koira",
+                            "answer": "Hond"
+                        }
+                    ]
+                }
+            ]
         },
         {
             "_id": "Aaron",
             "email": "aaron@gmail.com",
             "password": "$2a$10$JrWeSo2Y44VI2J8LnKLSwOtPkIyVEP5Y66HJ9cUIR3CX68vjFtPrC",
             "decks": [{
-                "name": "Engels woordjes",
-                "description": "english",
+                "name": "English to dutch verbs",
+                "description": "My english verbs",
                 "creatorId": "Aaron",
                 // "creatorId": ,
                 // "lastPlayedDate": ,
                 "status": "isEdited",
                 "flashcards": [
                     {
-                        "_id": "Apprende",
+                        "_id": "Apprende0",
                         "type": "Text only",
-                        "question": "Hello"
+                        "question": "Eating",
+                        "answer": "Eten"
                     },
                     {
                         "_id": "Apprende1",
                         "type": "Text only",
-                        "question": "Hello1"
+                        "question": "Walking",
+                        "answer": "Lopen"
                     },
                     {
                         "_id": "Apprende2",
                         "type": "Text only",
-                        "question": "Hello2"
+                        "question": "Running",
+                        "answer": "Rennen"
                     },
                 ]
             }]
