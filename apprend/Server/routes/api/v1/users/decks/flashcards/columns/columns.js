d9e22df813b482b59ceb4669efafdab926e3a48f
Aaron van den Berg <a.vdberg98@gmail.com>
2020-01-13 15:38:00 +0100

    Deleted unused route

--- apprend/Server/routes/api/v1/users/decks/flashcards/columns/columns.js
+++ apprend/Server/routes/api/v1/users/decks/flashcards/columns/columns.js
@@ -34,24 +34,6 @@ columns.patch('/:columnId', auth.user, async (req, res) => {
     }
 
 
-})
-
-columns.patch('/:columnId', auth.user, async (req, res) => {
-    try {
-        const { columnId } = req.params
-        const { column } = req.body
-        const data = {
-            index: columnId,
-            value: column.value
-        }
-        const result = await req.flashcard.editColumn(data)
-        req.user.markModified('decks')
-        await req.user.save()
-        return res.status(201).json({message: 'Changes saved' , data: result, success: true})
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({message: 'Something went wrong'})
-    }
-})
+});
 
 module.exports = columns
